7fbd1dabb5aa3603fac4b2d5bfd915b0272c3199
mdsahidparvez <sahidparvez786@gmail.com>
2021-02-15 10:30:13 +0545

    HMS

--- src/admin-login/admin-login.component.spec.ts
+++ src/admin-login/admin-login.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,7 +9,7 @@ describe('AdminLoginComponent', () => {
   let component: AdminLoginComponent;
   let fixture: ComponentFixture<AdminLoginComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AdminLoginComponent ]
     })
